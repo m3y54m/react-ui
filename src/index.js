bf2d179ff00509c4a86b0f6a054902abc4e8590c
Meysam Parvizi <meysam.parvizi.ir@gmail.com>
2021-07-11 20:51:38 +0430

    Write 'Hello World'

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// https://reactjs.org/blog/2020/09/22/introducing-the-new-jsx-transform.html
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+var element = <div>Hello World!</div>
+
+ReactDOM.render(element, document.getElementById('root'));
