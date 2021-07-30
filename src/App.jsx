6a00ace898cd2d7204417900526f4362d701bedf
Meysam Parvizi <meysam.parvizi.ir@gmail.com>
2021-07-31 03:46:40 +0430

    Change theme of Navbar from light to dark

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ class NavBar extends Component {
   render() {
     return (
       <React.Fragment>
-        <Navbar bg="light" expand="lg">
+        <Navbar bg="dark" variant="dark" expand="lg">
           <Container>
             <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
