c6eebbd98c15977ce1368b9708f2d5f1a97ca274
Meysam Parvizi <meysam.parvizi.ir@gmail.com>
2021-07-31 03:38:35 +0430

    Add React-Bootstrap Navbar

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 // https://reactjs.org/blog/2020/09/22/introducing-the-new-jsx-transform.html
 
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react"
+import ReactDOM from "react-dom"
 
 // Bootstrap CSS
-// The following line can be included in your src/index.js or App.js file
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-import NavBar from './App';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 // Custom CSS
 import "./index.css";
 
+import NavBar from "./App";
+
 // Renders NavBar component in the <div> tag with the id of 'root'
 // in the index.html file
-ReactDOM.render(<NavBar />, document.getElementById('root'));
+ReactDOM.render(
+    <NavBar />,
+    document.getElementById("root")
+);
 
