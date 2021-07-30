d7b21678e005fe30c1d0786fa52f44b94e92769d
Meysam Parvizi <meysam.parvizi.ir@gmail.com>
2021-07-31 04:27:25 +0430

    Render expressions

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Component } from "react";
-import MainContent from "./MainContent";
-
 // import NavBar component from NavBar.jsx file
 import NavBar from "./NavBar";
+// import MainContent component from MainContent.jsx file
+import MainContent from "./MainContent";
 
 export default class App extends Component {
   // Every component must have a method called "render"
--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -1,7 +1,19 @@
 import { Component } from "react";
 
-export default class MainContent extends Component{
-    render(){
-        return <div>Hello from MainContent!</div>
-    }
+export default class MainContent extends Component {
+
+  // dynamic content
+  state = { appTitle: "Hello Buddy!" }
+
+  render() {
+    return (
+      <div>
+        {/* Use Bootstrap predefined classes for styling */}
+        <h4 className="border-bottom m-1 p-1">
+          {/* Print out the dynamic content using expressions */}
+          {this.state.appTitle}
+        </h4>
+      </div>
+    );
+  }
 }
