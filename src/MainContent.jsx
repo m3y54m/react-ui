0bf46041ad01e38ae4c989d0339e312a3cac1f13
Meysam Parvizi <meysam.parvizi.ir@gmail.com>
2021-07-31 04:55:09 +0430

    Handle events

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -1,19 +1,32 @@
+import React from "react";
 import { Component } from "react";
-
+import { Badge } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 export default class MainContent extends Component {
 
   // dynamic content
-  state = { appTitle: "Hello Buddy!" }
+  state = { pageTitle: "Customers", customersCount: 5 }
 
   render() {
     return (
       <div>
         {/* Use Bootstrap predefined classes for styling */}
-        <h4 className="border-bottom m-1 p-1">
+        <h3 className="border-bottom m-1 p-1">
           {/* Print out the dynamic content using expressions */}
-          {this.state.appTitle}
-        </h4>
+          {this.state.pageTitle}
+          <Badge className="m-2 bg-danger rounded-pill">
+            {this.state.customersCount}
+          </Badge>
+          <Button variant="primary" onClick={this.onRefreshClick}>
+            Refresh
+          </Button>
+        </h3>
       </div>
     );
+  }
+
+  // Executes when the user clicks on Refresh button
+  onRefreshClick() {
+    console.log("Refresh clicked");
   }
 }
